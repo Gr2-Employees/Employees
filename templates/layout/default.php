59ed6b1f4bb682be0bbfe1e1b682f753b87047e8
hendrthomas <75525068+hendrthomas@users.noreply.github.com>
2020-12-07 16:11:39 +0100

    fermeture balise a

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -41,7 +41,7 @@ $cakeDescription = 'CakePHP: the rapid development php framework';
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav ml-5 mt-3">
             <li class="nav-item">
-                <a class="nav-link" href="<?= $this->Url->build('/') ?>">Home</a
+                <a class="nav-link" href="<?= $this->Url->build('/') ?>">Home</a>
 
             </li>
             <li class="nav-item">
