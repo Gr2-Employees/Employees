17e059a080190bc94c47028760339a1027ad5d33
Nathan D <nathandeltour2@gmail.com>
2020-12-10 23:05:21 +0100

    Added WomenAtWork charts

--- src/Controller/WomenAtWorkController.php
+++ src/Controller/WomenAtWorkController.php
@@ -17,6 +17,143 @@ class WomenAtWorkController extends AppController
     public function index()
     {
         /**
+         * Comparaison du nombre de femmes et d'hommes dans l'entreprise
+         */
+        $query = $this->getTableLocator()->get('employees')->find();
+
+        $query->select([
+            'count' => $query->func()->count('employees.emp_no'),
+            'employees.gender'
+        ])
+            ->group([
+                'employees.gender'
+            ]);
+
+        $result = $query->all();
+
+        $nbMen = 0;
+        $nbWomen = 0;
+
+        foreach ($result as $row) {
+            if ($row->gender === 'M') {
+                $nbMen = $row->count;
+            } else {
+                $nbWomen = $row->count;
+            }
+        }
+
+        $all = $nbWomen + $nbMen;
+
+        $pctMen = ($nbMen / $all) * 100;
+        $pctWomen = ($nbWomen / $all) * 100;
+        //var_dump($nbMen); // 179973
+        //var_dump($nbWomen); // 120051
+
+        $this
+            ->set(compact('pctWomen'))
+            ->set(compact('pctMen'));
+
+
+        /**
+         * Récupération du nombre de femmes par année
+         * 1990-1995-2000-2002
+         */
+        $arrNbWomen = [];
+        $arrYears = ['1990', '1995', '2000', '2002'];
+        $query = $this->getTableLocator()->get('dept_emp')->find();
+
+        $query->select([
+            'count' => $query->func()->count('*')
+        ])
+        ->join([
+            'em' => [
+                'table' => 'employees',
+                'conditions' => 'em.emp_no = dept_emp.emp_no'
+            ]
+        ])
+        ->where([
+            'em.gender' => 'F'
+        ])
+        ->where(
+            function ($exp) {
+                return $exp->like('to_date', '%1990%');
+            }
+        );
+
+        $arrNbWomen[] = $query->first()->count;
+
+        // 1995
+        $query = $this->getTableLocator()->get('dept_emp')->find();
+
+        $query->select([
+            'count' => $query->func()->count('*')
+        ])
+        ->join([
+            'em' => [
+                'table' => 'employees',
+                'conditions' => 'em.emp_no = dept_emp.emp_no'
+            ]
+        ])
+        ->where([
+            'em.gender' => 'F'
+        ])
+        ->where(
+            function ($exp) {
+                return $exp->like('to_date', '%1995%');
+            }
+        );
+
+        $arrNbWomen[] = $query->first()->count;
+
+        // 2000
+        $query = $this->getTableLocator()->get('dept_emp')->find();
+
+        $query->select([
+            'count' => $query->func()->count('*')
+        ])
+        ->join([
+            'em' => [
+                'table' => 'employees',
+                'conditions' => 'em.emp_no = dept_emp.emp_no'
+            ]
+        ])
+        ->where([
+            'em.gender' => 'F'
+        ])
+        ->where(
+            function ($exp) {
+                return $exp->like('to_date', '%2000%');
+            }
+        );
+
+        $arrNbWomen[] = $query->first()->count;
+
+        // 2002
+        $query = $this->getTableLocator()->get('dept_emp')->find();
+
+        $query->select([
+            'count' => $query->func()->count('*')
+        ])
+        ->join([
+            'em' => [
+                'table' => 'employees',
+                'conditions' => 'em.emp_no = dept_emp.emp_no'
+            ]
+        ])
+        ->where([
+            'em.gender' => 'F'
+        ])
+        ->where(
+            function ($exp) {
+                return $exp->like('to_date', '%2002%');
+            }
+        );
+
+        $arrNbWomen[] = $query->first()->count;
+        var_dump($arrNbWomen);
+        $this->set('arrWomen', $arrNbWomen)
+            ->set('arrYears', $arrYears);
+        /**
          * Récupération des femmes managers
          */
         $query = $this->getTableLocator()->get('employees')
@@ -31,14 +168,11 @@ class WomenAtWorkController extends AppController
                 'dema' => [
                     'table' => 'dept_manager',
                     'conditions' => 'dema.emp_no = employees.emp_no'
-                ]
-            ])
-            ->join([
+                ],
                 'emti' => [
                     'table' => 'employee_title',
                     'conditions' => 'emti.emp_no = employees.emp_no'
                 ],
-
             ])
             ->where([
                 'emti.title_no' => '7',
@@ -47,13 +181,15 @@ class WomenAtWorkController extends AppController
 
         $results = $query->all();
 
-        $tabManagF = array();
+        $femaleManager = [];
         $cpt = 0;
+
         foreach ($results as $row) {
-            $tabManagF[] = $row["first_name"] . " " . $row["last_name"];
+            $femaleManager[] = $row["first_name"] . " " . $row["last_name"];
             $cpt++;
-        };
-        var_dump($tabManagF);
+        }
+        var_dump($cpt);
+        var_dump($femaleManager);
     }
 
 
--- templates/WomenAtWork/index.php
+++ templates/WomenAtWork/index.php
@@ -12,54 +12,96 @@
     <?= $this->fetch('css') ?>
 </head>
 <body>
-    <div id="main">
-        <h1>Women At Work</h1>
-        <div id="presentationText">
-            <?= $this->Text->autoParagraph(
-                __('The organization began by offering a resource room equipped with job listings and library books
+<div id="main">
+    <h1>Women At Work</h1>
+    <div id="presentationText">
+        <?= $this->Text->autoParagraph(
+            __('The organization began by offering a resource room equipped with job listings and library books
                 on women’s employment as well as holding seminars and workshops on career fields and career planning classes.
                 For about three decades, our organization has provided job skill training, employment preparation and job search services to thousands of job seekers.')
-            ) ?>
+        ) ?>
 
-            <?= $this->Text->autoParagraph(
-                __('As per research, companies who have a higher number of women in their workforce have gained high financial
+        <?= $this->Text->autoParagraph(
+            __('As per research, companies who have a higher number of women in their workforce have gained high financial
                 profits and productivity as their output, when compared to the companies which have fewer women employees. Being in this 21st century, where women have been equally capable and successful as men, gender diversity at
                 the workplace should be made a mandatory rule.
                 Moreover, the companies which have a diverse workforce are more successful when compared to other companies which are mostly male-dominated.')
-            ) ?>
-        </div>
-
-        <!-- Affichage des graphiques / Charts -->
-        <div style="width:500px; height:350px;">
-            <h2>Nos statistiques</h2>
-            <h6>Lines</h6>
-            <canvas id="womenPieChart"></canvas>
-        </div>
+        ) ?>
     </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
-    <!-- Obligé de mettre ce script ici pour pouvoir manipuler les données de La DDB -->
-    <script>
-        const pieChart = document.getElementById('womenPieChart').getContext('2d');
-        const chart = new Chart(pieChart, {
-            // The type of chart we want to create
-            type: 'pie',
+    <!-- Affichage des graphiques / Charts -->
+    <div style="width:500px; height:350px;">
+        <h2>Nos statistiques</h2>
+        <h6>Lines</h6>
+        <canvas id="womenPieChart"></canvas>
+        <hr/>
+        <canvas id="womenLineChart"></canvas>
+    </div>
 
-            // The data for our dataset
-            data: {
-                labels: ['DepName1', 'DepName2', 'DepName3'],
-                datasets: [{
-                    label: 'Women Chart',
-                    backgroundColor: 'rgb(172,99,255)',
-                    borderColor: 'rgb(17,71,229)',
-                    data: [6, 20, 15] // NbWomen
-                }]
-            },
+</div>
 
-            // Configuration options go here
-            options: {}
-        });
-    </script>
+<script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
+<!-- Obligé de mettre ce script ici pour pouvoir manipuler les données de La DDB -->
+<script>
+    const pieChart = document.getElementById('womenPieChart').getContext('2d');
+    const chartPie = new Chart(pieChart, {
+        type: 'pie',
+        data: {
+            labels: ['Women (%)', 'Men (%)'],
+            datasets: [{
+                backgroundColor: [
+                    'rgb(241,178,255)',
+                    'rgb(143,230,250)',
+                ],
+                borderColor: 'rgb(255,255,255)',
+                data: [
+                    <?= $this->Number->format($pctWomen, [
+                        'precision' => '2'
+                    ]) ?>,
+                    <?= $this->Number->format($pctMen, [
+                        'precision' => '2'
+                    ])?>
+                ]
+            }]
+        },
+        options: {
+            title: {
+                display: 'true',
+                text: 'Proportion de femmes et d’hommes dans l\'entreprise'
+            }
+        }
+    });
+</script>
+<script>
+    const lineChart = document.getElementById('womenLineChart').getContext('2d');
+    const chartLine = new Chart(lineChart, {
+        type: 'line',
+
+        // The data for our dataset
+        data: {
+            labels: [<?php foreach ($arrYears as $year) {
+                echo $year . ',';
+            } ?>
+            ],
+            datasets: [{
+                backgroundColor: 'transparent',
+                borderColor: 'rgb(252,138,233)',
+                data: [<?php foreach ($arrWomen as $nb) {
+                    echo $nb . ',';
+                } ?>
+                ]
+            }]
+        },
+
+        // Configuration options go here
+        options: {
+            title: {
+                display: 'true',
+                text: 'Évolution du nombre de femmes par année'
+            }
+        }
+    });
+</script>
 </body>
 </html>
 
