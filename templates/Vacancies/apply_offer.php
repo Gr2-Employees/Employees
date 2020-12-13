a2ba33362053a8f32044f8ecc25b2a5b9cc62dbf
Nathan D <nathandeltour2@gmail.com>
2020-12-13 23:00:28 +0100

    Finished HireMe form

--- templates/Vacancies/apply_offer.php
+++ templates/Vacancies/apply_offer.php
@@ -9,32 +9,49 @@
         <h2> <?= __('Hire me') ?></h2>
     </div>
 
-    <?php if (!$isFormSent) { ?>
-        <!-- Hire me Form -->
+    <?= $this->Flash->render() ?>
+
+    <?php if ($showForm) { ?>
+        <!-- Hire-me Form -->
         <?= $this->Form->create(null, [
             'url' => [
                 'action' => 'applyOffer'
             ]
         ]) ?>
 
-        <!-- Surname -->
+        <!-- Hidden dept_no field-->
+        <?= $this->Form->hidden('dept_no', [
+            'value' => $dept_no
+        ])?>
+
+        <!-- Hidden title_no field-->
+        <?= $this->Form->hidden('title_no', [
+            'value' => $title_no
+        ]) ?>
+
+        <!-- Surname Input -->
         <?= $this->Form->control('surname', [
             'id' => 'surnameField',
             'type' => 'text',
             'placeholder' => __('Enter your surname')
         ]) ?>
 
-        <!-- Lastname -->
+        <!-- Lastname Input -->
         <?= $this->Form->control('lastname', [
             'id' => 'lastnameField',
             'type' => 'text',
             'placeholder' => __('Enter your lastname')
         ]) ?>
 
-        <!-- Email -->
+        <!-- Email Input -->
         <?= $this->Form->control('email', [
             'type' => 'email',
             'placeholder' => 'Your Email'
+        ]) ?>
+
+        <!-- Birthdate Field - TODO:Change format -->
+        <?= $this->Form->control('birthdate', [
+            'type' => 'date',
         ]) ?>
 
         <!-- Text Area -->
@@ -49,12 +66,19 @@
         <?= $this->Form->control('c-v', [
             'type' => 'file'
         ]) ?>
+
         <!-- Submit button -->
         <?= $this->Form->button('Submit') ?>
 
         <!-- End Form -->
         <?= $this->Form->end() ?>
+    <?php } else { ?>
+        <!-- Link back to homepage -->
+        <div id="mailOutput">
+            <?= $this->Html->link( __('Back Home'), '/', [
+                'controller' => 'Pages',
+                'action' => 'display'
+            ]) ?>
+        </div>
     <?php } ?>
-
-    <!-- TODO: Add Elements to tell user the form has been sent -->
 </div>
