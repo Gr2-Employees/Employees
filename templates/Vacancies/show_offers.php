9fe5dc15170c030385bda64df54b79644a170885
Nathan D <nathandeltour2@gmail.com>
2020-12-14 17:45:28 +0100

    Fix singulier/pluriel showOffers

--- templates/Vacancies/show_offers.php
+++ templates/Vacancies/show_offers.php
@@ -8,7 +8,11 @@
     <div id="TitlePage">
         <h2>Page Hire</h2>
         <?php if (isset($vacancyName) && !is_null($vacancies)) { ?>
-        <h4> <?= __('Vacant position(s) in department of ') . h($vacancyName) ?></h4>
+        <?php if($nbVacancies === 1) { ?>
+            <h4> <?= __('Vacant position in department of ') . h($vacancyName) ?></h4>
+        <?php } else { ?>
+            <h4> <?= __('Vacant positions in department of ') . h($vacancyName) ?></h4>
+        <?php } ?>
     </div>
 
     <div id="tableVacancies">
@@ -23,18 +27,18 @@
                     <td><?= $vacancy->amount ?></td>
                     <td>
                         <?php if ($vacancy->amount !== '0') {
-                            echo $this->Html->link(__('Hire me !'), 
-                            [
-                                'action' => 'applyOffer',
-                                '?' => [
-                                    'title_no' => $vacancy->title_no,
-                                    'dept_no' => $vacancy->dept_no
-                                ]
-                            ],
-                            [
-                                'class' => 'btn btn-success',
-                                'style' => 'float: right'
-                            ]);
+                            echo $this->Html->link(__('Hire me !'),
+                                [
+                                    'action' => 'applyOffer',
+                                    '?' => [
+                                        'title_no' => $vacancy->title_no,
+                                        'dept_no' => $vacancy->dept_no
+                                    ]
+                                ],
+                                [
+                                    'class' => 'btn btn-success',
+                                    'style' => 'float: right'
+                                ]);
                         } ?>
                     </td>
                 </tr>
