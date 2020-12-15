1955a14aaced5c74bf02eba689f1d81998cf8ca9
Radad-J <radad@gmail.com>
2020-12-15 21:15:54 +0100

    add new css for form login

--- templates/Users/login.php
+++ templates/Users/login.php
@@ -1,4 +1,4 @@
-<div class="users form">
+<div id="form-login" class="users form">
     <?= $this->Flash->render() ?>
     <h3>Login</h3>
     <?= $this->Form->create() ?>
@@ -7,7 +7,7 @@
         <?= $this->Form->control('email', ['required' => true]) ?>
         <?= $this->Form->control('password', ['required' => true]) ?>
     </fieldset>
-    <?= $this->Form->submit(__('Login')); ?>
+    <?= $this->Form->submit(__('Login'), ["id"=>"btn-form-login"]); ?>
     <?= $this->Form->end() ?>
 
     <?= $this->Html->link("Add User", ['action' => 'add']) ?>
