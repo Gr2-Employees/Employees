2c2303286e99e714c51f658c1889643d133ada89
Radad-J <radad@gmail.com>
2020-12-17 16:16:39 +0100

    update about us page

--- templates/Pages/about_us.php
+++ templates/Pages/about_us.php
@@ -1,16 +1,12 @@
-    <div>
-        <div>
-            <h2>Who are we?</h2>
-            <p>United Suite, est un conglomérat de plusieurs petites banques qui ont décidée de s'unir lors de la crise bancaire de mai 1873. <br>
-
-                <br>Fondée en juillet 1872, la Banque United Suite n'a cessé, au cours des nonantes années qui ont suivi, d'acquérir des actionnaires, de racheter ou s'unir avec d'autres banques implantées dans les principales villes belges.<br>
-                En 1931, United Suite s'engagée non seulement dans les activités bancaires traditionnelles, mais également dans la gestion d'intérêts industriels principalement localisés en Belgique et en Afrique.<br>
-                Suite aux réformes bancaires intervenues en Belgique en 1934 et 1935, l'opération consacrait la séparation des activités bancaires et des intérêts de la société de portefeuille.<br>
-
-                <br>La Banque United Suite trouve son origine dans la réunion de trois entreprise bancaire principales, crée par les familles Hendrickx-Deltour-El Jaidi, actives dans le secteur bancaire depuis l'indépendance de la Belgique, en 1830.<br>
-                Après 1945, la Banque a développé rapidement ses activités, notamment par le biais de rachat d'autre banques.<br>
-
-                <br>La United Suite réunis en assemblée générale extraordinaire le 17 avril 2003, ses actionnaires ont modifié sa raison sociale en United Suite Belgique SA.</p>
-
+    <div id="who-are-we-background" class="row">
+        <div id="who-are-we-content" class="col-6">
+                <h2 style="text-align: center">Qui sommes-nous ?</h2>
+                <p>United Suite, est un conglomérat de plusieurs petites banques qui ont décidé de s'unir lors de la crise bancaire de mai 1873.</p>
+                <p>Fondée en juillet 1872, la Banque United Suite n'a cessé, au cours des nonantes années qui ont suivi, d'acquérir des actionnaires, de racheter ou s'unir avec d'autres banques implantées dans les principales villes belges.</p><br>
+                <p>En 1931, United Suite s'engagea non seulement dans les activités bancaires traditionnelles, mais également dans la gestion d'intérêts industriels principalement localisés en Belgique et en Afrique.</p>
+                <p>Suite aux réformes bancaires intervenues en Belgique en 1934 et 1935, l'opération consacrait la séparation des activités bancaires et des intérêts de la société de portefeuille.</p>
+                <p>La Banque United Suite trouve son origine dans la réunion de trois entreprises bancaires principales, créé par les familles Hendrickx-Deltour-El Jaidi, actives dans le secteur bancaire depuis l'indépendance de la Belgique, en 1830.</p><br>
+                <p>Après 1945, la Banque a développé rapidement ses activités, notamment par le biais de rachats d'autres banques.</p>
+                <p>La United Suite réunie en assemblée générale extraordinaire le 17 avril 2003, ses actionnaires ont modifié sa raison sociale en United Suite Belgique SA.</p>
         </div>
     </div>
