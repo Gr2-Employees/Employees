84ddde3a1ed518d7b6d15ec3d1e87c92b3ae7287
Nathan D <nathandeltour2@gmail.com>
2020-12-20 15:45:24 +0100

    Added dashboard

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller\Admin;
+
+use App\Controller\AppController;
+use App\Model\Entity\Department;
+use App\Model\Table\DepartmentsTable;
+use Cake\Datasource\Exception\RecordNotFoundException;
+use Cake\Datasource\ResultSetInterface;
+use Cake\Http\Response;
+
+/**
+ * Departments Controller
+ *
+ * @property DepartmentsTable $Departments
+ * @method Department[]|ResultSetInterface paginate($object = null, array $settings = [])
+ */
+class DashboardController extends AppController
+{
+    /**
+     * Index method
+     *
+     * @return Response|null|void Renders view
+     */
+    public function index()
+    {
+        $welcomeMessage = '<h1>Welcome in the back-office!</h1>';
+
+        $this->set(compact('welcomeMessage'));
+    }
+}
---
+++ templates/Admin/Dashboard/index.php
@@ -0,0 +1,8 @@
+<?php
+
+if (isset($welcomeMessage)) {
+    echo $welcomeMessage;
+}
+?>
+
+<!-- TODO: Ajouter des cells pour des quick infos sur les différentes parties du back-office -->
