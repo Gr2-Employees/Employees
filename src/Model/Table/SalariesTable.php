2d4245abee63ea24afe656519f2e39c22a7c002d
Nathan D <nathandeltour2@gmail.com>
2020-12-21 17:58:30 +0100

    Added links to back-office

--- src/Model/Table/SalariesTable.php
+++ src/Model/Table/SalariesTable.php
@@ -3,6 +3,9 @@ declare(strict_types=1);
 
 namespace App\Model\Table;
 
+use App\Model\Entity\Salary;
+use Cake\Datasource\EntityInterface;
+use Cake\Datasource\ResultSetInterface;
 use Cake\ORM\Query;
 use Cake\ORM\RulesChecker;
 use Cake\ORM\Table;
@@ -11,19 +14,19 @@ use Cake\Validation\Validator;
 /**
  * Salaries Model
  *
- * @method \App\Model\Entity\Salary newEmptyEntity()
- * @method \App\Model\Entity\Salary newEntity(array $data, array $options = [])
- * @method \App\Model\Entity\Salary[] newEntities(array $data, array $options = [])
- * @method \App\Model\Entity\Salary get($primaryKey, $options = [])
- * @method \App\Model\Entity\Salary findOrCreate($search, ?callable $callback = null, $options = [])
- * @method \App\Model\Entity\Salary patchEntity(\Cake\Datasource\EntityInterface $entity, array $data, array $options = [])
- * @method \App\Model\Entity\Salary[] patchEntities(iterable $entities, array $data, array $options = [])
- * @method \App\Model\Entity\Salary|false save(\Cake\Datasource\EntityInterface $entity, $options = [])
- * @method \App\Model\Entity\Salary saveOrFail(\Cake\Datasource\EntityInterface $entity, $options = [])
- * @method \App\Model\Entity\Salary[]|\Cake\Datasource\ResultSetInterface|false saveMany(iterable $entities, $options = [])
- * @method \App\Model\Entity\Salary[]|\Cake\Datasource\ResultSetInterface saveManyOrFail(iterable $entities, $options = [])
- * @method \App\Model\Entity\Salary[]|\Cake\Datasource\ResultSetInterface|false deleteMany(iterable $entities, $options = [])
- * @method \App\Model\Entity\Salary[]|\Cake\Datasource\ResultSetInterface deleteManyOrFail(iterable $entities, $options = [])
+ * @method Salary newEmptyEntity()
+ * @method Salary newEntity(array $data, array $options = [])
+ * @method Salary[] newEntities(array $data, array $options = [])
+ * @method Salary get($primaryKey, $options = [])
+ * @method Salary findOrCreate($search, ?callable $callback = null, $options = [])
+ * @method Salary patchEntity(EntityInterface $entity, array $data, array $options = [])
+ * @method Salary[] patchEntities(iterable $entities, array $data, array $options = [])
+ * @method Salary|false save(EntityInterface $entity, $options = [])
+ * @method Salary saveOrFail(EntityInterface $entity, $options = [])
+ * @method Salary[]|ResultSetInterface|false saveMany(iterable $entities, $options = [])
+ * @method Salary[]|ResultSetInterface saveManyOrFail(iterable $entities, $options = [])
+ * @method Salary[]|ResultSetInterface|false deleteMany(iterable $entities, $options = [])
+ * @method Salary[]|ResultSetInterface deleteManyOrFail(iterable $entities, $options = [])
  */
 class SalariesTable extends Table
 {
--- templates/Admin/Dashboard/index.php
+++ templates/Admin/Dashboard/index.php
@@ -5,4 +5,28 @@ if (isset($welcomeMessage)) {
 }
 ?>
 
-<!-- TODO: Ajouter des cells pour des quick infos sur les différentes parties du back-office -->
+    <!-- TODO: Ajouter des cells pour des quick infos sur les différentes parties du back-office -->
+    <!-- Lien vers admin/dept/index-->
+<?= $this->Html->link('To departments', [
+    'prefix' => 'Admin',
+    'controller' => 'Departments',
+    'action' => 'index'
+]) ?>
+
+<hr/>
+
+    <!-- Lien vers admin/Employees/index-->
+<?= $this->Html->link('To Employees', [
+    'prefix' => 'Admin',
+    'controller' => 'Employees',
+    'action' => 'index'
+]) ?>
+
+<hr/>
+
+    <!-- Lien vers admin/Users/index-->
+<?= $this->Html->link('To Users', [
+    'prefix' => 'Admin',
+    'controller' => 'users',
+    'action' => 'index'
+]) ?>
