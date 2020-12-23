bfe40147ccc7a05ce9a8f96210db93e45ffbf1d8
Radad-J <radad@gmail.com>
2020-12-23 17:26:44 +0100

    Finition design Edit department

--- templates/Admin/Departments/edit.php
+++ templates/Admin/Departments/edit.php
@@ -4,53 +4,44 @@
  * @var \App\Model\Entity\Department $department
  */
 ?>
-<div class="row">
-    <aside class="column">
-        <div class="side-nav">
-            <h4 class="heading"><?= __('Actions') ?></h4>
-            <!-- Button delete admin/dept-->
-            <?= $this->Form->postLink(__('Delete'), [
-                'prefix' => 'Admin',
-                'action' => 'delete',
-                $department->dept_no
-            ], [
-                'confirm' => __('Are you sure you want to delete # {0}?',
-                    $department->dept_no),
-                'class' => 'side-nav-item'
-            ]) ?>
-
-            <!-- Button admin/dept/index -->
-            <?= $this->Html->link(__('List Departments'), [
-                'prefix' => 'Admin',
-                'action' => 'index'
-            ], [
-                'class' => 'side-nav-item'
-            ]) ?>
-        </div>
-    </aside>
-    <div class="column-responsive column-80">
-        <div class="departments form content">
+<div class="row"
+     style="min-height:91vh;background-image: url('/img/login-background-min.jpg');background-size: cover;background-position: center;background-repeat: no-repeat">
+    <div class="column-responsive column-80" style="margin: 50px auto auto auto;">
+        <div class="departments form content" style="position: relative">
             <?= $this->Form->create($department, ["type" => "file"]) ?>
-            <fieldset>
+            <fieldset class="row">
+
                 <legend><?= __('Edit Department') ?></legend>
-                <?php
-                echo $this->Form->control('dept_name');?>
+                <!-- Button admin/dept/index -->
+                <?= $this->Html->link(__('List Departments'), [
+                    'prefix' => 'Admin',
+                    'action' => 'index'
+                ], [
+                    'style' => 'position: absolute;right:20px;top:30px;padding:10px;background-color:#2A6496;border-radius:3px;color:white;font-size:1.8rem'
+                ]) ?>
+
                 <div class="col-6">
+                    <?php
+                    echo $this->Form->control('dept_name');
+                    echo $this->Form->control('description');
+                    echo $this->Form->control('address');
+                    echo $this->Form->control('rules');
+                    ?>
+                </div>
+                <div class="col-6" style="text-align: center;padding-top: 30px">
                     <?= $this->Html->image('/img/uploads/dept_pictures/' . $department->picture, [
                         'alt' => 'Manager du département ' . $department->dept_name . '.',
-                        'class' => 'manager-picture'
+                        'class' => 'manager-picture',
+                        'style' => 'margin-bottom: 20px'
                     ]) ?>
+
+                    <?php
+                    echo $this->Form->control('picture', [
+                        'type' => 'file', 'required' => 'false'
+                    ]); ?>
                 </div>
-                <?php
-                echo $this->Form->control('picture', [
-                'type' => 'file','required'=>'false',
-                ]);
-                echo $this->Form->control('description');
-                echo $this->Form->control('address');
-                echo $this->Form->control('rules');
-                ?>
             </fieldset>
-            <?= $this->Form->button(__('Submit')) ?>
+            <?= $this->Form->button(__('Submit'), ['style' => "margin-left: 15px;background-color: #2A6496;border-color: #2A6496"]) ?>
             <?= $this->Form->end() ?>
         </div>
     </div>
