92688c6565f8853364c09042859ef32251648d00
Radad-J <radad@gmail.com>
2020-12-23 20:00:24 +0100

    Finition user templates design

--- templates/Admin/Users/edit.php
+++ templates/Admin/Users/edit.php
@@ -6,22 +6,25 @@
 ?>
 <div class="row"
      style="min-height:90vh;background-image: url('/img/login-background-min.jpg');background-size: cover;background-position: center;background-repeat: no-repeat">
-    <aside style="width: 15%">
-        <div class="side-nav">
-            <h4 class="heading"><?= __('Actions') ?></h4>
-            <?= $this->Form->postLink(
+            <!--<?=$this->Form->postLink(
                 __('Delete'),
                 ['action' => 'delete', $user->id],
                 ['confirm' => __('Are you sure you want to delete # {0}?', $user->id), 'class' => 'side-nav-item']
-            ) ?>
-            <?= $this->Html->link(__('List Users'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
-        </div>
-    </aside>
-    <div style="width: 75%">
+            )?>-->
+    <div class="col-7" style="margin:90px auto auto">
         <div class="users form content">
             <?= $this->Form->create($user) ?>
-            <fieldset>
+            <fieldset style="position: relative">
                 <legend><?= __('Edit User') ?></legend>
+                <!-- Button admin/dept/index -->
+                <?= $this->Html->link(__('List Users'), [
+                    'prefix' => 'Admin',
+                    'action' => 'index'
+                ], [
+                    'class' => 'button', 'style' => "background-color: #2A6496;border-color: #2A6496;position: absolute;top: 0px;right: 0px"
+
+                ]) ?>
+
                 <?php
                     echo $this->Form->control('email');
                     echo $this->Form->control('password');
