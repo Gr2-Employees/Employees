39b8c0d3ddcec49b90de3640f06250bc3a1a5dc2
Radad-J <radad@gmail.com>
2020-12-24 20:21:26 +0100

    Revert Prettify CSS and HTML -- users/edit

--- templates/Admin/Users/edit.php
+++ templates/Admin/Users/edit.php
@@ -4,25 +4,23 @@
  * @var \App\Model\Entity\User $user
  */
 ?>
-<div class="row"
-     style="min-height:90vh;background-image: url('/img/login-background-min.jpg');background-size: cover;background-position: center;background-repeat: no-repeat">
+<div class="row row row-styled-background">
             <!--<?=$this->Form->postLink(
                 __('Delete'),
                 ['action' => 'delete', $user->id],
                 ['confirm' => __('Are you sure you want to delete # {0}?', $user->id), 'class' => 'side-nav-item']
             )?>-->
-    <div class="col-7" style="margin:90px auto auto">
+    <div class="col-7 m-auto"">
         <div class="users form content">
             <?= $this->Form->create($user) ?>
-            <fieldset style="position: relative">
+            <fieldset class="position-relative">
                 <legend><?= __('Edit User') ?></legend>
                 <!-- Button admin/dept/index -->
                 <?= $this->Html->link(__('List Users'), [
                     'prefix' => 'Admin',
                     'action' => 'index'
                 ], [
-                    'class' => 'button', 'style' => "background-color: #2A6496;border-color: #2A6496;position: absolute;top: 0px;right: 0px"
-
+                    'class' => 'button btn-blue position-absolute', 'style' => "top: 0px;right: 0px"
                 ]) ?>
 
                 <?php
@@ -30,7 +28,7 @@
                     echo $this->Form->control('password');
                 ?>
             </fieldset>
-            <?= $this->Form->button(__('Submit'), ['style' => "background-color: #2A6496;border-color: #2A6496"]) ?>
+            <?= $this->Form->button(__('Submit'), ['class' => "btn-blue"]) ?>
             <?= $this->Form->end() ?>
         </div>
     </div>
