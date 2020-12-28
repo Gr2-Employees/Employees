e1475fb8064813924a2e444a2d5d8ffe12071962
Radad-J <radad1998@gmail.com>
2020-12-28 18:02:06 +0100

    Add table raise salary

--- templates/Demands/index.php
+++ templates/Demands/index.php
@@ -6,21 +6,21 @@
 ?>
 <div class="demands index content">
     <?= $this->Html->link(__('New Demand'), ['action' => 'add'], ['class' => 'button btn-blue float-right']) ?>
-    <h3><?= __('Demands') ?></h3>
+    <h3><?= __('Department change demands') ?></h3>
     <div class="table-responsive">
         <table>
             <thead>
             <tr>
-                <th><?= $this->Paginator->sort('id') ?></th>
-                <th><?= $this->Paginator->sort('emp_no') ?></th>
-                <th><?= $this->Paginator->sort('type') ?></th>
-                <th><?= $this->Paginator->sort('about') ?></th>
-                <th><?= $this->Paginator->sort('status') ?></th>
+                <th><?= __('id') ?></th>
+                <th><?= __('emp_no') ?></th>
+                <th><?= __('type') ?></th>
+                <th><?= __('about') ?></th>
+                <th><?= __('status') ?></th>
                 <th class="actions"><?= __('Actions') ?></th>
             </tr>
             </thead>
             <tbody>
-            <?php foreach ($demands as $demand): ?>
+            <?php foreach ($demandsDepartment as $demand) { ?>
                 <tr>
                     <td><?= $this->Number->format($demand->id) ?></td>
                     <td><?= $this->Number->format($demand->emp_no) ?></td>
@@ -28,31 +28,31 @@
                     <td><?= h($demand->about) ?></td>
                     <td><?= h($demand->status) ?></td>
                     <td class="actions">
-                        <?php if ($demand->status === 'in progress' && $this->Identity->get('role') !== 'admin') { ?>
+                        <?php if ($this->Identity->get('role') !== 'admin') { ?>
+                            <?php if ($demand->status !== 'approved') { ?>
+                                <!-- Link to Approve demand -->
+                                <?= $this->Form->postLink(__('Approve'), [
+                                    'controller' => 'Demands',
+                                    'action' => 'approve', $demand->id
+                                ], [
+                                    'confirm' => __('Are you sure you want to approve # {0}?',
+                                        $demand->id),
+                                    'class' => 'button',
+                                    'style' => 'color:white;background-color:green;border-color:green'
 
-                            <!-- Link to Approve demand -->
-                            <?= $this->Form->postLink(__('Approve'), [
-                                'controller' => 'Demands',
-                                'action' => 'approve', $demand->id
-                            ], [
-                                'confirm' => __('Are you sure you want to approve # {0}?',
-                                    $demand->id),
-                                'class' => 'button',
-                                'style' => 'color:white;background-color:green;border-color:green'
+                                ]) ?>
 
-                            ]) ?>
-
-                            <!-- Link to Decline demand -->
-                            <?= $this->Form->postLink(__('Decline'), [
-                                'controller' => 'Demands',
-                                'action' => 'decline', $demand->id
-                            ], [
-                                'confirm' => __('Are you sure you want to decline # {0}?',
-                                    $demand->id),
-                                'class' => 'button',
-                                'style' => 'color:white;'
-                            ]) ?>
-
+                                <!-- Link to Decline demand -->
+                                <?= $this->Form->postLink(__('Decline'), [
+                                    'controller' => 'Demands',
+                                    'action' => 'decline', $demand->id
+                                ], [
+                                    'confirm' => __('Are you sure you want to decline # {0}?',
+                                        $demand->id),
+                                    'class' => 'button',
+                                    'style' => 'color:white;'
+                                ]) ?>
+                            <?php } ?>
                         <?php } else {
                             if ($this->Identity->get('role') === 'admin') {
                                 // Link to Delete demand
@@ -62,7 +62,7 @@
                                     $demand->id
                                 ], [
                                     'confirm' => __('Are you sure you want to delete # {0}?',
-                                    $demand->id),
+                                        $demand->id),
                                     'class' => 'button',
                                     'style' => 'color:white'
                                 ]);
@@ -70,18 +70,89 @@
                         } ?>
                     </td>
                 </tr>
-            <?php endforeach; ?>
+            <?php } ?>
             </tbody>
         </table>
     </div>
-    <div class="paginator">
-        <ul class="pagination">
-            <?= $this->Paginator->first('<< ' . __('first')) ?>
-            <?= $this->Paginator->prev('< ' . __('previous')) ?>
-            <?= $this->Paginator->numbers() ?>
-            <?= $this->Paginator->next(__('next') . ' >') ?>
-            <?= $this->Paginator->last(__('last') . ' >>') ?>
-        </ul>
-        <p><?= $this->Paginator->counter(__('Page {{page}} of {{pages}}, showing {{current}} record(s) out of {{count}} total')) ?></p>
+
+    <div class="table-responsive mt-4">
+        <h3><?= __('Raise salary demands') ?></h3>
+        <!-- Table raise pour le comptable-->
+        <?php if ($this->Identity->get('role') === 'comptable' || $this->Identity->get('role') === 'admin') { ?>
+            <table>
+                <thead>
+                <tr>
+                    <th><?= __('id') ?></th>
+                    <th><?= __('emp_no') ?></th>
+                    <th><?= __('type') ?></th>
+                    <th><?= __('about') ?></th>
+                    <th><?= __('status') ?></th>
+                    <th><?= __('amount') ?></th>
+                    <th class="actions"><?= __('Actions') ?></th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php foreach ($demandsRaise as $demand) { ?>
+                    <tr>
+                        <td><?= $this->Number->format($demand->id) ?></td>
+                        <td><?= $this->Number->format($demand->emp_no) ?></td>
+                        <td><?= h($demand->type) ?></td>
+                        <td><?= h($demand->about) ?></td>
+                        <td><?= h($demand->status) ?></td>
+                        <?php if ($demand->type === 'raise') { ?>
+                            <td><?= $this->Number->toPercentage(h($demand->amount)) ?></td>
+                        <?php } else { ?>
+                            <td><?= __('NULL') ?></td>
+                        <?php } ?>
+                        <td class="actions">
+                            <?php if ($demand->status === 'in progress' && $this->Identity->get('role') !== 'admin') { ?>
+
+                                <!-- Link to Approve raise demand -->
+                                <?= $this->Form->postLink(__('Approve'), [
+                                    'controller' => 'Demands',
+                                    'action' => 'approve', $demand->id,
+                                    '?' => [
+                                        'type' => 'raise'
+                                    ]
+                                ], [
+                                    'confirm' => __('Are you sure you want to approve # {0}?',
+                                        $demand->id),
+                                    'class' => 'button',
+                                    'style' => 'color:white;background-color:green;border-color:green'
+
+                                ]) ?>
+
+                                <!-- Link to Decline raise demand -->
+                                <?= $this->Form->postLink(__('Decline'), [
+                                    'controller' => 'Demands',
+                                    'action' => 'decline', $demand->id
+                                ], [
+                                    'confirm' => __('Are you sure you want to decline # {0}?',
+                                        $demand->id),
+                                    'class' => 'button',
+                                    'style' => 'color:white;'
+                                ]) ?>
+
+                            <?php } else {
+                                if ($this->Identity->get('role') === 'admin') {
+                                    // Link to Delete raise demand
+                                    echo $this->Form->postLink(__('Delete'), [
+                                        'controller' => 'Demands',
+                                        'action' => 'delete',
+                                        $demand->id
+                                    ], [
+                                        'confirm' => __('Are you sure you want to delete # {0}?',
+                                            $demand->id),
+                                        'class' => 'button',
+                                        'style' => 'color:white'
+                                    ]);
+                                }
+                            } ?>
+                        </td>
+                    </tr>
+                <?php } ?>
+                </tbody>
+            </table>
+        <?php } ?>
     </div>
 </div>
