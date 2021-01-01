13be8021587e8709b5a366a2b7146aadc0cb2cba
Nathan D <nathandeltour2@gmail.com>
2021-01-01 23:00:35 +0100

    admin/employee/add form

--- templates/Admin/Employees/add.php
+++ templates/Admin/Employees/add.php
@@ -7,21 +7,67 @@
 <div class="row row-styled-background">
     <div class="column-responsive column-80 mt-4 mx-auto">
         <div class="employees form content position-relative p-5"">
-            <?= $this->Form->create($employee) ?>
+            <?= $this->Form->create($employee, [
+                'type' => 'file'
+            ]) ?>
             <fieldset class="position-relative">
                 <legend><?= __('Add Employee') ?></legend>
-                <?= $this->Html->link(__('List Employees'), ['action' => 'index'],
-                    ['class' => 'button position-absolute', 'style' => "background-color: #2A6496;border-color: #2A6496;top: 0px;right: 0px"]) ?>
+                <?= $this->Html->link(__('List Employees'), [
+                    'action' => 'index'
+                ], [
+                    'class' => 'button position-absolute',
+                    'style' => "background-color: #2A6496;border-color: #2A6496;top: 0px;right: 0px"
+                ]) ?>
 
-                <?php
-                echo $this->Form->control('birth_date');
-                echo $this->Form->control('first_name');
-                echo $this->Form->control('last_name');
-                echo $this->Form->control('gender');
-                echo $this->Form->control('hire_date');
-                echo $this->Form->control('picture');
-                echo $this->Form->control('email');
-                echo $this->Form->control('departments._ids', ['options' => $departments]);
+            <?php
+                // Firstname field
+                echo $this->Form->control('first_name', [
+                    'required' => 'true'
+                ]);
+
+                // Lastname field
+                echo $this->Form->control('last_name', [
+                    'required' => 'true'
+                ]);
+
+                // Gender field
+                echo $this->Form->control('gender', [
+                    'type' => 'select',
+                    'options' => [
+                        'M' => 'M',
+                        'F' => 'F',
+                        'Other' => 'Other'
+                    ]
+                ]);
+
+                // Birthday field
+                echo $this->Form->control('birth_date', [
+                    'type' => 'date',
+                    'required' => 'true'
+                ]);
+
+                // Email field
+                echo $this->Form->control('email', [
+                    'required' => 'true'
+                ]);
+
+                // Hire date field
+                echo $this->Form->control('hire_date', [
+                    'type' => 'date',
+                    'required' => 'true'
+                ]);
+
+                // Department select field
+                echo $this->Form->control('departments._ids', [
+                    'type' => 'select',
+                    'empty' => __('Choose a department'),
+                    'options' => $departments
+                ]);
+
+                // Picture field
+                echo $this->Form->control('picture', [
+                    'type' => 'file'
+                ]);
                 ?>
             </fieldset>
             <?= $this->Form->button(__('Submit'), ['class' => "btn-blue"]) ?>
